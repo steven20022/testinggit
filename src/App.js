26000d9e81bb176979824ed4bcf9fe1083966601
steven20022 <jacobelam11@gmail.com>
2023-02-23 10:22:52 -0500

    Version 4

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from "react";
 import CountDown from "react-countdown"
 
 function Completionist(){
-  <span>You are good to go!</span>
+  <span style={{
+    color : 'red',
+    fontWeight: 'bold',
+    fontSize: 52}}>You are good to go!</span>
 }
 
 function renderer({hours, minutes, seconds, completed}){
@@ -15,9 +18,21 @@ function renderer({hours, minutes, seconds, completed}){
 
 function App() {
   return (
-    <div>
+    <div style={{
+      textAlign: 'center',
+      fontWeight: 'bold',
+      backgroundColor: 'black',
+      color: 'white',
+      fontSize: 52,
+      height: '100hv',
+      overflow: 'auto'
+    }}>
       <h2>Countdown</h2>
-      <div>
+      <div style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}>
         <CountDown
           date={Date.now() + 10000}
           renderer={renderer}
