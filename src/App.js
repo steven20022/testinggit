f65307befa777275d3cfbfcf55ef927f42cf23e7
steven20022 <jacobelam11@gmail.com>
2023-02-23 10:03:23 -0500

    Version 3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import React from "react";
+import CountDown from "react-countdown"
+
+function Completionist(){
+  <span>You are good to go!</span>
+}
+
+function renderer({hours, minutes, seconds, completed}){
+  if (completed) {
+    return <Completionist />;
+  } else {
+    return <span>{hours}:{minutes}:{seconds}</span>
+  }
+}
 
 function App() {
   return (
     <div>
       <h2>Countdown</h2>
+      <div>
+        <CountDown
+          date={Date.now() + 10000}
+          renderer={renderer}
+        >
+          <Completionist />
+        </CountDown>
+      </div>
     </div>
   );
 }
